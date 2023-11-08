9afdcc05a22545fb9c35fae5df6748e968c28ef0
GingerBreadMuncher <2KenBlock@gmail.com>
2023-11-09 04:46:10 +0600

    calcFunctions prep - THE PATH TO THE GREAT FOREVER

---
+++ GingerBreadCalculator/CalcFunctions.cs
@@ -0,0 +1,83 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Controls;
+using System.Windows;
+
+namespace GingerBreadCalculator
+{
+    public partial class CalcFunctions
+    {
+        string operationOutput = "";
+        string output = "";
+        string operation = "";
+        string lastOperation = "";
+        double temp = 0;
+        bool resultGiven = false;
+
+        private void Btn_Click(object sender, RoutedEventArgs e)
+        {
+            string name = ((Button)sender).Name;
+
+            string value = ((Button)sender).Content.ToString();
+
+
+            if (resultGiven == false)
+            {
+                output += value;
+                OutputTextBlock.Text = output;
+            }
+            else
+            {
+                Clear();
+                output = value.ToString();
+                OutputTextBlock.Text = output;
+                resultGiven = false;
+            }
+
+            if (name == "ClearBtn")
+            {
+                Clear();
+            }
+
+            if (output == "0")
+            {
+                resultGiven = true;
+            }
+        }
+
+        private void MathOperation(object sender, RoutedEventArgs e)
+        {
+            string name = ((Button)sender).Name;
+
+            string value = ((Button)sender).Content.ToString();
+
+            void MathOp()
+            {
+                operationOutput = temp + " " + value + " ";
+                OperationTextBlock.Text = operationOutput;
+            }
+
+            lastOperation = value;
+
+            if (output != "")
+            {
+                temp = double.Parse(output);
+                output = "";
+                operation = name;
+                MathOp();
+                resultGiven = false;
+            }
+        }
+
+        void OperationBtn(double _outputTemp)
+        {
+            operationOutput = temp + " " + lastOperation + " " + output;
+            OperationTextBlock.Text = operationOutput;
+            output = _outputTemp.ToString();
+            OutputTextBlock.Text = output;
+        }
+    }
+}
