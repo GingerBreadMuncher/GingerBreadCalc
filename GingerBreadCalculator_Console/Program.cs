720cb09b77fb4b7ef49aaf353d43d1b96049c1d0
Andrey <EpicSharpBlade@gmail.com>
2023-11-08 23:37:44 -0500

    Create API and Console Project in Solution

---
+++ GingerBreadCalculator_Console/Program.cs
@@ -0,0 +1,17 @@
+﻿using CalculationAPI;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace GingerBreadCalculator_Console
+{
+    public class Program
+    {
+        static void Main(string[] args)
+        {
+        }
+
+    }
+}
