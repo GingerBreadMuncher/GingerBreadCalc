e97643cf01faef60de9292697836d874ae62a565
Andrey <EpicSharpBlade@gmail.com>
2023-11-08 23:40:29 -0500

    Console project developed

--- GingerBreadCalculator_Console/Program.cs
+++ GingerBreadCalculator_Console/Program.cs
@@ -11,7 +11,43 @@ namespace GingerBreadCalculator_Console
     {
         static void Main(string[] args)
         {
+            ConsoleEngine engine = new ConsoleEngine();
+            while (true)
+            {
+                engine.Action();
+            }
         }
 
+        public class ConsoleEngine : IWindowInterface
+        {
+            public void Action()
+            {
+                Console.WriteLine("Enter first number:");
+                double i = double.Parse(Console.ReadLine());
+                Console.WriteLine("Enter operation sign:");
+                string sign = Console.ReadLine();
+                Console.WriteLine("Enter second number:");
+                double j = double.Parse(Console.ReadLine());
+
+                CalcFunctions.SelectNumber("", i.ToString(), this);
+                CalcFunctions.MathOperation(GetBtnName(sign), sign, this);
+                CalcFunctions.SelectNumber("", i.ToString(), this);
+                CalcFunctions.GetResult(this);
+                Console.WriteLine("\n");
+            }
+
+            public string GetBtnName(string s)
+            {
+                if (s == "+") return "PlusBtn";
+                if (s == "-") return "MinusBtn";
+                if (s == "*") return "MultyplyBtn";
+                if (s == "/") return "DivideBtn";
+                return "";
+            }
+
+            public void OperationOutput(string _text) => Output(_text);
+
+            public void Output(string _text) => Console.Write(_text);
+        }
     }
 }
